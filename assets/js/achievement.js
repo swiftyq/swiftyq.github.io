593f95e02e190c8d2a29cdc1c9a4122717e9bb43
johnr0 <johnr0hol@gmail.com>
2017-11-09 20:33:06 +0900

    ACHIEVEMENT

--- assets/js/achievement.js
+++ assets/js/achievement.js
@@ -18,19 +18,33 @@ var list_text = [
   ''
 ]
 
+var grid_colors = ["#aaff91",
+"#75d5ff",
+"#eeeeee"]
 $(document).ready(function(){
   generate_grid();
   generate_list();
-  achievement_pop_up();
+  achievement_pop_up("hoihoi", "dullinun");
 })
 generate_grid = function(){
   for(var i=0; i<5; i++){
     $("#achievement_grid_box").append("<div id='grid_row_"+i.toString()+"' class='grid_row row'></div>")
     for(var j=0; j<10; j++){
       $("#grid_row_"+i.toString()).append("<div id='grid_"+i.toString()+"_"+j.toString()+"' class='col-sm grid'></div>")
+      var rand = Math.floor(Math.random()*3)
+      var rand2 = Math.floor(Math.random()*100000)+100000
+      //https://d30y9cdsu7xlg0.cloudfront.net/png/149821-200.png
+      $("#grid_"+i.toString()+"_"+j.toString()).css("background-color", grid_colors[rand]).append("<img class='grid_img' src='https://d30y9cdsu7xlg0.cloudfront.net/png/"+rand2.toString()+"-200.png' width='0' height='0'></img>")
     }
   }
   $(".grid").outerHeight(function(){
+    var height = $(this).width();
+    var oh = $(this).outerWidth();
+    //console.log($(this).outerWidth())
+    $(".grid_img").outerWidth(height).outerHeight(height).css("margin-top", function(){
+      return (oh-height)/2;
+    });
+
     return $(this).outerWidth();
   })
 }
@@ -46,13 +60,25 @@ generate_list = function(){
     .append("<div id='list_content_text_"+i.toString()+"' class='achievement_entity_text'></div>")
     $("#list_content_head_"+i.toString()).text(list_head[i]);
     $("#list_content_text_"+i.toString()).text(list_text[i]);
+    var rand2 = Math.floor(Math.random()*100000)+100000
+    //https://d30y9cdsu7xlg0.cloudfront.net/png/149821-200.png
+    $("#list_image_sub_"+i.toString()).append("<img class='list_img' src='https://d30y9cdsu7xlg0.cloudfront.net/png/"+rand2.toString()+"-200.png' width='0' height='0'></img>")
+
   }
   $("#list_sub_0").css("border-top-left-radius", "30px")
   .css("border-top-right-radius", "30px");
+
   $('.achievement_entity_image').outerHeight(function(){
     return $(this).outerWidth();
   })
   $('.achievement_entity_sub_image').outerHeight(function(){
+    var height = $(this).width();
+    var oh = $(this).outerWidth();
+    //console.log($(this).outerWidth())
+    $(".list_img").outerWidth(height).outerHeight(height).css("margin-top", function(){
+      return (oh-height)/2;
+    });
     return $(this).outerWidth();
   })
+
 }
