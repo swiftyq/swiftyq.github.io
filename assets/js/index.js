1ae5dcb9db9b494373ae789da6b7a29528a3484a
kyungjejo <kyungjejo@gmail.com>
2017-11-09 20:33:11 +0900

    index with all prompts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
 $("u").on('click', function() {
 	$(".modal").modal('show');
 })
+
+$( ".dropdown-menu a" ).on('click', function() {
+  console.log($(this).text());
+  var target_input = $( ".form-group input[type='expertise']" )
+  var old_val = target_input.val()
+  target_input.val(old_val+$(this).text()+", ");
+})
+
+$( "#btn-signup" ).on('click', function() {
+  $( ".modal.fade" ).show();
+  alert("clicked");
+})
+
+$( "#verylastbtn" ).on('click', function() {
+  window.location.href = "index.html";
+})
