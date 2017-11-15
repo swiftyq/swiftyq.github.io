44bdf609471ece4cad58e117f5e9c7edf3155394
johnr0 <johnr0hol@gmail.com>
2017-11-15 21:44:10 +0900

    request page

--- static/js/index.js
+++ static/js/index.js
@@ -8,7 +8,10 @@ $( ".dropdown-menu a" ).on('click', function() {
   var old_val = target_input.val()
   target_input.val(old_val+$(this).text()+", ");
 })
-
+$("#field_reset").on("click", function(){
+	 $('#expertise_button').prop('disabled', false);
+	 $("#expertise").val('');
+})
 $( "#btn-signup" ).on('click', function() {
   $( ".modal.fade" ).show();
   alert("clicked");
