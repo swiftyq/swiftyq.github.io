a8e688f3eb99f89b126dd9643da9d5aea83ec6b4
chaec0803 <chaec0803@gmail.com>
2017-11-20 23:09:13 +0900

    updated request inbox

--- server.py
+++ server.py
@@ -1,112 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template
-from flask import request
-from flask_socketio import SocketIO, send
-from subprocess import call
-from time import time
-from math import log
-import json
-import achievement_list_opener
-import sqlite3
-
-conn = sqlite3.connect('./static/db/userinfo.db',check_same_thread=False)
-cur = conn.cursor()
-
-#cur. execute('CREATE TABLE inboxinfo (id text, pic text, expertise text)')
-#cur.execute('''CREATE TABLE info
-#				(id text, password text, name text, expertise text)''')
-#cur.execute('''CREATE TABLE rating
-#				(id text, rating number, date text)''')
-#cur.execute('''CREATE TABLE achievement
-#				(id text, achievement number, date text)''')
-#cur.execute('''CREATE TABLE session''')
-
-app = Flask(__name__)
-socket_io = SocketIO(app)
-
-# Data structure
-# User-info: id, password, expertise
-# rating history,
-# achievement: id (text), achievement (number), date (text)
-# session history
-achievement_l = achievement_list_opener.returner()
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/',methods=["POST"])
-def login():
-	if request.method == "POST":
-		user_id = request.form['id']
-		password = request.form['password']
-		cur.execute("SELECT  * from info where id=? and password=?", (user_id,password,))
-		user_info = cur.fetchone()
-		if not user_info:
-			warning = "Incorrect id or password. Please try again."
-			return render_template("index.html", warning=warning)
-		return extract(user_id)
-	#return render_template("inbox.html", user_id = user_id)
-def extract(user_id):
-	cur.execute("SELECT expertise from info where id=?", (user_id,))
-	expertise = cur.fetchone()
-	#if not expertise:
-		#print("no")
-	return render_template("inbox.html", user_id = user_id, expertise = expertise[0])
-
-@app.route('/signup')
-def signup():
-    return render_template("signup.html")
-
-@app.route('/signedup', methods=["POST"])
-def signedup():
-	print(request.form)
-	user_id = request.form['id']
-	password = request.form['password']
-	name =  request.form['name']
-	expertise = request.form['expertise']
-	expertise = expertise.split(",")[:-1]
-	for e in expertise:
-		cur.execute("INSERT INTO info VALUES (?,?,?,?)", (user_id,password,name,e))
-	conn.commit()
-	return render_template("index.html")
-
-@app.route('/inbox', methods = ["POST"])
-def inbox():
-	if(request.method == "GET"):
-		return render_template("inbox.html")
-	if(request.method == "POST"):
-		print(request.form['user_id'])
-		#return render_template("inbox.html")
-
-
-	#if(request.method == "POST"):
-		#return render_template("inbox.html")
-
-@app.route('/chat')
-def chat():
-    return render_template("chat.html")
-
-@app.route('/achievement')
-def achievement():
-    return render_template("achievement.html")
-
-@app.route('/request_page')
-def request_page():
-    return render_template("request.html")
-
-@app.route('/achievement_list', methods=['POST'])
-def achievement_list():
-    return achievement_l
-
-if __name__ == '__main__':
-    socket_io.run(app, host='0.0.0.0', debug=True, port=5000)
-=======
-=======
->>>>>>> f0fdb46a145d0fdf62a0858e4b5b0dd9c21a2fd1
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
@@ -162,19 +53,22 @@ def login():
 		if not user_info:
 			warning = "Incorrect id or password. Please try again."
 			return render_template("index.html", warning=warning)
-<<<<<<< HEAD
-		return extract(user_id)
-	#return render_template("inbox.html", user_id = user_id)
-=======
 	return extract(user_id)
 
->>>>>>> f0fdb46a145d0fdf62a0858e4b5b0dd9c21a2fd1
 def extract(user_id):
 	cur.execute("SELECT expertise from info where id=?", (user_id,))
 	expertise = cur.fetchone()
 	#if not expertise:
 		#print("no")
-	return render_template("inbox.html", user_id = user_id, expertise = expertise[0])
+	for x in range(0, 3):
+		cur.execute("INSERT INTO request VALUES (?,?,?,?,?,?)", ("1", "hi!", "john", "john", expertise[0], "11/11",))
+	cur.execute("SELECT * from request where expertise = ?", (expertise[0],))
+	rtable = cur.fetchall()
+	cur.execute("SELECT COUNT(id) from request where expertise = ?", (expertise[0],))
+	count = cur.fetchone()[0]
+	mylist = []
+
+	return render_template("inbox.html", user_id = user_id, expertise = expertise[0], rtable=rtable, count = count)
 
 @app.route('/signup')
 def signup():
