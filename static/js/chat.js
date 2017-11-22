b2fc135130ea139752651c515c1eb30396218f99
chaec0803 <chaec0803@gmail.com>
2017-11-23 01:31:34 +0900

    final 23

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,15 @@
-$( "#send" ).on('click', function() {
-  var text = $( "textarea#chat_text" ).text();
-
-})
+ $("#prompt form").submit(function(e) {
+ 
+      // close the overlay
+      triggers.eq(1).overlay().close();
+ 
+      // get user input
+      var input = $("input", this).val();
+ 
+      // do something with the answer
+      triggers.eq(1).html(input);
+ 
+      // do not submit the form
+      return e.preventDefault();
+  });
+  
